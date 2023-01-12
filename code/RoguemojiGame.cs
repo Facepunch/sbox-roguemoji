bc4de4e879f8738d2c65e2773a0961c148c12581
ryleigh <ryleighkostash@gmail.com>
2023-01-11 18:46:08 -0800

    windows 10 emoji font

--- code/RoguemojiGame.cs
+++ code/RoguemojiGame.cs
@@ -75,7 +75,7 @@ public partial class RoguemojiGame : GameManager
             UnidentifiedScrollNames = new List<string>() { "WYZ'LOK", "MYR'KLYN", "PHYZGRYF", "XORPHYX", "GRYFAD", "RYXORK", "ORAXUM", "ZORKOZAL", "KLYNX", "QYN", "ARPHYNY", "LUZ'ROKLUM", "YNDRYNY", "PYG'JYG", "BRAX'PHY", "FEN'XOR", "CIRXYX" };
             UnidentifiedScrollNames.Shuffle();
 
-            UnidentifiedPotionSymbols = new List<string>() { "🉑", "🉐", "⚪️", "🟤", "🔘", "🧿", "🌐", "🌓" }; // ㊗️ ㊙️
+            UnidentifiedPotionSymbols = new List<string>() { "🉑", "🉐", "⚫️", "🟤", "🔘", "🧿", "🌐", "🌓", "🌑", "🌕️", "🌙",  }; // ㊗️ ㊙️
             UnidentifiedPotionSymbols.Shuffle();
             UnidentifiedPotionNames = new List<string>() { "cloudy", "misty", "murky", "sparkling", "fizzy", "bubbly", "smoky", "congealed", "chalky", "radiant", "milky", "thick", "pasty", "glossy", "dull", "dusty", "syrupy", "pungent", 
                 "viscous", "sludgy", "pale", "filmy", "rusty", "chunky", "creamy", "hazy", "silky" };
